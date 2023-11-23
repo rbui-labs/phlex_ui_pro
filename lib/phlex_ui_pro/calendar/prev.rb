ac90dbe8c68eac22dbda084b18077f098036ff52
george kettle <georgekettle.gk@gmail.com>
2023-11-23 13:44:28 +1100

    Change all styles to use css variables

--- lib/phlex_ui_pro/calendar/prev.rb
+++ lib/phlex_ui_pro/calendar/prev.rb
@@ -34,7 +34,7 @@ module PhlexUI
                 name: "previous-month",
                 aria_label: "Go to previous month",
                 class:
-                    "rdp-button_reset rdp-button inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input hover:bg-accent-background hover:text-accent-text h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute left-1",
+                    "rdp-button_reset rdp-button inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input hover:bg-accent hover:text-accent-foreground h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute left-1",
                 type: "button",
                 data_action: "click->calendar#prevMonth"
             }
